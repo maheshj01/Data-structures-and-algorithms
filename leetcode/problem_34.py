15deb74246d1f838b438ba2fc0e92b9e5b7b34f5
mahesh jamdade <maheshmn121@gmail.com>
2026-08-11 22:35:49 -0400

    add a alternate solution

--- leetcode/problem_34.py
+++ leetcode/problem_34.py
@@ -17,3 +17,76 @@ class Solution:
         result[1] = i
         return result
 
+
+# Solution 2: Binary Search
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+# Idea is we will run binary search twice once to find the first position and once to find the last.
+# if finding first position, we will move the right pointer to the middle - 1 until we find the first position.
+# if finding last position, we will move the left pointer to the middle + 1 until we find the last position.
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # l            m            r
+        # [5, 7, 7, 8, 8, 8, 8, 8, 10]
+        #  0           4         8
+        
+        # l   m      r 
+        # [5, 7, 7, 8, 8, 8, 8, 8, 10]
+        #  0        3  
+        
+        #        l  r 
+        # [5, 7, 7, 8, 8, 8, 8, 8, 10]
+        #        2  3  
+        
+        #           lr 
+        # [5, 7, 7, 8, 8, 8, 8, 8, 10]
+        #        2  3  
+        
+        #              l     m      r 
+        # [5, 7, 7, 8, 8, 8, 8, 8, 10]
+        #        2  3  
+        
+        #                       l    r 
+        # [5, 7, 7, 8, 8, 8, 8, 8, 10]
+        #        2  3  
+        
+        #  l = 0
+        #  r = 8
+        # target = 8
+        # mid = 4, nums[mid] = 8 
+        # find left = true
+        #  
+        #  l   m   r   
+        
+
+        def binSearch(firstTarget):
+            left = 0
+            right = len(nums) - 1
+            result = -1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
+                    result = mid
+                    if firstTarget:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                elif(nums[mid] < target):
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return result
+        
+        # mid = binSearch(0, len(nums) - 1)
+        # left = binSearch(0, mid)
+        # right = binSearch(mid + 1, len(nums) - 1)
+        result1 = binSearch(True)
+        if(result1 == -1):
+            return [-1, -1] 
+        result2 = binSearch(False)
+        return [result1, result2]
+
+            
+       
+
+
